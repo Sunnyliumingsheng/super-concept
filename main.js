d45062cf086ca1c3d50f18a3956ed00df7e7c0fb
sunny刘名升 <liumingsheng3411@lilith>
2025-09-04 20:27:19 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+import { app, BrowserWindow } from 'electron'
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+// sqlite3 verbose 可以打印调试信息
+sqlite3.verbose();
+
+async function initDatabase() {
+  // 打开数据库，如果不存在会自动创建
+  const db = await open({
+    filename: './mydatabase.db',
+    driver: sqlite3.Database
+  });
+
+  // 创建表
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      age INTEGER
+    )
+  `);
+
+  return db;
+}
+initDatabase();
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
